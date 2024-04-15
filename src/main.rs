4f52ff19305eb1a148a44c10beb0d71bce914f04
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-04-15 16:43:30 +0200

    Add decryption

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,30 @@ use age_plugin::{
     recipient::{self, RecipientPluginV1},
     run_state_machine, Callbacks,
 };
+
 use card_backend_pcsc::PcscBackend;
 use clap::Parser;
 use openpgp_card::Card;
+use x25519_dalek::{EphemeralSecret, PublicKey, StaticSecret};
 
 use std::collections::HashMap;
 use std::io;
 
+use age_core::{
+    format::FILE_KEY_BYTES,
+    primitives::{aead_decrypt, aead_encrypt, hkdf},
+    secrecy::{ExposeSecret, SecretString},
+};
+
+// Use lower-case HRP to avoid https://github.com/rust-bitcoin/rust-bech32/issues/40
+const SECRET_KEY_PREFIX: &str = "age-secret-key-";
+const PUBLIC_KEY_PREFIX: &str = "age";
+
+pub const X25519_RECIPIENT_TAG: &str = "X25519";
+const X25519_RECIPIENT_KEY_LABEL: &[u8] = b"age-encryption.org/v1/X25519";
+
+pub const EPK_LEN_BYTES: usize = 32;
+pub const ENCRYPTED_FILE_KEY_BYTES: usize = FILE_KEY_BYTES + 16;
 struct RecipientPlugin;
 
 impl RecipientPluginV1 for RecipientPlugin {
@@ -44,6 +61,20 @@ impl RecipientPluginV1 for RecipientPlugin {
 
 struct IdentityPlugin;
 
+use base64::{prelude::BASE64_STANDARD_NO_PAD, Engine};
+pub(crate) fn base64_arg<A: AsRef<[u8]>, const N: usize, const B: usize>(
+    arg: &A,
+) -> Option<[u8; N]> {
+    if N > B {
+        return None;
+    }
+
+    let mut buf = [0; B];
+    match BASE64_STANDARD_NO_PAD.decode_slice(arg, buf.as_mut()) {
+        Ok(n) if n == N => Some(buf[..N].try_into().unwrap()),
+        _ => None,
+    }
+}
 impl IdentityPluginV1 for IdentityPlugin {
     fn add_identity(
         &mut self,
@@ -59,7 +90,60 @@ impl IdentityPluginV1 for IdentityPlugin {
         files: Vec<Vec<Stanza>>,
         mut callbacks: impl Callbacks<identity::Error>,
     ) -> io::Result<HashMap<usize, Result<FileKey, Vec<identity::Error>>>> {
-        panic!()
+        let stanza = files[0][0];
+        if stanza.tag != X25519_RECIPIENT_TAG {
+            panic!("return None;");
+        }
+
+        // Enforce valid and canonical stanza format.
+        // https://c2sp.org/age#x25519-recipient-stanza
+        let ephemeral_share = match &stanza.args[..] {
+            [arg] => match base64_arg::<_, EPK_LEN_BYTES, 33>(arg) {
+                Some(ephemeral_share) => ephemeral_share,
+                None => panic!("return Some(Err(DecryptError::InvalidHeader)),"),
+            },
+            _ => panic!("return Some(Err(DecryptError::InvalidHeader)),"),
+        };
+        if stanza.body.len() != ENCRYPTED_FILE_KEY_BYTES {
+            panic!("return Some(Err(DecryptError::InvalidHeader));");
+        }
+
+        let epk: PublicKey = ephemeral_share.into();
+        let encrypted_file_key: [u8; ENCRYPTED_FILE_KEY_BYTES] = stanza.body[..]
+            .try_into()
+            .expect("Length should have been checked above");
+
+        let pk: PublicKey = (&self.0).into();
+        let shared_secret = self.0.diffie_hellman(&epk);
+        // Replace with `SharedSecret::was_contributory` once x25519-dalek supports newer
+        // zeroize (https://github.com/dalek-cryptography/x25519-dalek/issues/74#issuecomment-1159481280).
+        if shared_secret
+            .as_bytes()
+            .iter()
+            .fold(0, |acc, b| acc | b)
+            .ct_eq(&0)
+            .into()
+        {
+            panic!("return Some(Err(DecryptError::InvalidHeader));");
+        }
+
+        let mut salt = [0; 64];
+        salt[..32].copy_from_slice(epk.as_bytes());
+        salt[32..].copy_from_slice(pk.as_bytes());
+
+        let enc_key = hkdf(&salt, X25519_RECIPIENT_KEY_LABEL, shared_secret.as_bytes());
+
+        // A failure to decrypt is non-fatal (we try to decrypt the recipient
+        // stanza with other X25519 keys), because we cannot tell which key
+        // matches a particular stanza.
+        aead_decrypt(&enc_key, FILE_KEY_BYTES, &encrypted_file_key)
+            .ok()
+            .map(|mut pt| {
+                // It's ours!
+                let file_key: [u8; FILE_KEY_BYTES] = pt[..].try_into().unwrap();
+                pt.zeroize();
+                Ok(file_key.into())
+            })
     }
 }
 
