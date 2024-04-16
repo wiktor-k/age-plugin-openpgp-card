694bf8b412dc6652a57b1221b3972a9749d14700
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-04-16 11:34:45 +0200

    Commit working code

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use age_plugin::{
 };
 use card_backend_pcsc::PcscBackend;
 use clap::Parser;
-use openpgp_card::Card;
+use openpgp_card::{crypto_data::PublicKeyMaterial, Card};
 use subtle::ConstantTimeEq;
 use x25519_dalek::{EphemeralSecret, PublicKey};
 
@@ -82,7 +82,8 @@ impl IdentityPluginV1 for IdentityPlugin {
         plugin_name: &str,
         bytes: &[u8],
     ) -> Result<(), identity::Error> {
-        todo!()
+        eprintln!("add_identity: {index} {plugin_name} {bytes:?}");
+        Ok(())
     }
 
     fn unwrap_file_keys(
@@ -91,6 +92,7 @@ impl IdentityPluginV1 for IdentityPlugin {
         mut callbacks: impl Callbacks<identity::Error>,
     ) -> io::Result<HashMap<usize, Result<FileKey, Vec<identity::Error>>>> {
         let stanza = &files[0][0];
+        eprintln!("stanza {stanza:?}");
         if stanza.tag != X25519_RECIPIENT_TAG {
             panic!("return None;");
         }
@@ -114,12 +116,38 @@ impl IdentityPluginV1 for IdentityPlugin {
             .expect("Length should have been checked above");
 
         //        let pk: PublicKey = (&self.0).into();
-        let pk = "test";
-        let shared_secret = "test"; //self.0.diffie_hellman(&epk);
-                                    // Replace with `SharedSecret::was_contributory` once x25519-dalek supports newer
-                                    // zeroize (https://github.com/dalek-cryptography/x25519-dalek/issues/74#issuecomment-1159481280).
+        let backend = PcscBackend::cards(None)
+            .expect("cards")
+            .into_iter()
+            .next()
+            .expect("one card");
+        let mut card = Card::new(backend.expect("backend")).expect("card");
+        let mut tx = card.transaction().expect("tx");
+        let pk: Vec<u8> = if let PublicKeyMaterial::E(ecc) = tx
+            .public_key(openpgp_card::KeyType::Decryption)
+            .expect("pk")
+        {
+            ecc.data().into()
+        } else {
+            panic!("not ecc key");
+        };
+        tx.verify_pw1_user(
+            callbacks
+                .request_secret("plz unlock")?
+                .expect("secret")
+                .expose_secret()
+                .as_bytes(),
+        )
+        .expect("verify to work");
+        let shared_secret = tx
+            .decipher(openpgp_card::crypto_data::Cryptogram::ECDH(
+                &ephemeral_share,
+            ))
+            .expect("decipher to work");
+        //let shared_secret = "test"; //self.0.diffie_hellman(&epk);
+        // Replace with `SharedSecret::was_contributory` once x25519-dalek supports newer
+        // zeroize (https://github.com/dalek-cryptography/x25519-dalek/issues/74#issuecomment-1159481280).
         if shared_secret
-            .as_bytes()
             .iter()
             .fold(0, |acc, b| acc | b)
             .ct_eq(&0)
@@ -130,9 +158,9 @@ impl IdentityPluginV1 for IdentityPlugin {
 
         let mut salt = [0; 64];
         salt[..32].copy_from_slice(epk.as_bytes());
-        salt[32..].copy_from_slice(pk.as_bytes());
+        salt[32..].copy_from_slice(&pk[..]);
 
-        let enc_key = hkdf(&salt, X25519_RECIPIENT_KEY_LABEL, shared_secret.as_bytes());
+        let enc_key = hkdf(&salt, X25519_RECIPIENT_KEY_LABEL, &shared_secret);
 
         // A failure to decrypt is non-fatal (we try to decrypt the recipient
         // stanza with other X25519 keys), because we cannot tell which key
@@ -159,16 +187,43 @@ struct PluginOptions {
     age_plugin: Option<String>,
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = PluginOptions::parse();
 
     if let Some(state_machine) = opts.age_plugin {
         // The plugin was started by an age client; run the state machine.
-        return run_state_machine(
+        return Ok(run_state_machine(
             &state_machine,
             Some(|| RecipientPlugin),
             Some(|| IdentityPlugin),
-        );
+        )?);
+    }
+    use bech32::{ToBase32, Variant};
+    const IDENTITY_PREFIX: &str = "age-plugin-openpgp-card-";
+
+    const PUBLIC_KEY_PREFIX: &str = "age";
+
+    for backend in PcscBackend::cards(None)? {
+        let mut card = Card::new(backend?)?;
+        let mut tx = card.transaction()?;
+        //tx.application_related_data()?;
+        //tx.verify_pw1_user(&"12345".as_bytes())?;
+        //tx.decipher(openpgp_card::crypto_data::Cryptogram::ECDH())
+        //
+        if let PublicKeyMaterial::E(ecc) = tx.public_key(openpgp_card::KeyType::Decryption)? {
+            eprintln!(
+                "# {}",
+                bech32::encode(PUBLIC_KEY_PREFIX, ecc.data().to_base32(), Variant::Bech32)
+                    .expect("HRP is valid")
+            );
+
+            eprintln!(
+                "Encoded: {}",
+                bech32::encode(IDENTITY_PREFIX, &[1, 2, 3].to_base32(), Variant::Bech32,)
+                    .expect("bech to work")
+                    .to_uppercase()
+            );
+        }
     }
 
     // Here you can assume the binary is being run directly by a user,
